178a6a987c1b0d17286bb08a1bd182fcf6c6abdc
KLiebrecht1 <89344375+KLiebrecht1@users.noreply.github.com>
2026-02-04 08:20:32 +0100

    Update lht65n_ttn_decoder.js

--- lht65n_ttn_decoder.js
+++ lht65n_ttn_decoder.js
@@ -1,13 +1,15 @@
 function decodeUplink(input) {
+
   let external = false; // set to true if external temp. sensor connected
   var extTemp = 0;
+
   // ------------- BATTERY ---------------
   let batBytes = input.bytes[0] << 8 | input.bytes[1];
   let batStateValue = batBytes >> 14; // get first 2 bits of 2 battery bytes
   var batStateMsg = "";
   switch(batStateValue) {
     case 0:
-      batStateMsg = "Ultra Low";
+      batStateMsg = "Critical";
       break;
     case 1:
       batStateMsg = "Low";
@@ -18,8 +20,8 @@ function decodeUplink(input) {
     case 3:
       batStateMsg = "Good";
       break;
-} 
-  let batV = (batBytes & 0x3FFF)/ 1000; // get last 14 bit of 2 bytes
+  } 
+  let batV = (batBytes & 0x3FFF) / 1000; // get last 14 bit of 2 bytes
   
   // ------------- INTERNAL TEMP ---------------
   let intTempBytes = input.bytes[2] << 8 | input.bytes[3];
@@ -27,13 +29,30 @@ function decodeUplink(input) {
   let intTemp=(intNegFlag+(intTempBytes)) / 100;
 
   // ------------- INTERNAL HUMIDITY ---------------
-
   let intHumBytes = input.bytes[4] << 8 | input.bytes[5];
   let intHum = intHumBytes / 10;
   
-  // ------------- EXTERNAL TEMP. ---------------
+  // ------------- CALCULATE DEW POINT ---------------
+  var sdd = 0;
+  var dd = 0;
+  var eln = 0;
+  var td = 0;
+
+  if (intTemp >= 0) {
+    sdd = 6.1078*Math.pow(10,((7.5*intTemp)/(237.3+intTemp)));
+    dd = intHum/100.0*sdd;
+    eln = Math.log10(dd/6.1078);
+    td = 237.3*eln/(7.5-eln);
+  } else {
+    sdd = 6.1078*Math.pow(10,((7.6*intTemp)/(240.7+intTemp)));
+    dd = intHum/100.0*sdd;
+    eln = Math.log10(dd/6.1078);
+    td = 240.7*eln/(7.6-eln);
+  }
+  let DewPoint = (Math.round(100*td))/100;
   
-  if(external){
+  // ------------- EXTERNAL TEMP. ---------------
+  if (external) {
     let extSensorByte = input.bytes[6];
     let extTempBytes = input.bytes[7] << 8 | input.bytes[8];
     if (extSensorByte == 1 && external){
@@ -43,27 +62,29 @@ function decodeUplink(input) {
   }
   
   // ------------- OUTPUT ---------------
-  
-  if(!external){
+  if (!external) {
     return {
       data: {
-        batState: batStateMsg,
+        batState: batStateValue,
+        batStateMsg: batStateMsg,
         batV_V: batV,
         intTemp_C: intTemp,
         intHum_pct: intHum,
+        intDewPoint_C: DewPoint
       }
     };
-  }
-  
-  else{
+  } else {
     return {
       data: {
-        batState: batStateMsg,
+        batState: batStateValue,
+        batStateMsg: batStateMsg,
         batV_V: batV,
         intTemp_C: intTemp,
         intHum_pct: intHum,
+        intDewPoint_C: DewPoint,
         extTemp_C: extTemp
       }
     };
   }
-  }
+
+}
